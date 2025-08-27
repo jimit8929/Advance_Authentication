4a9adec19e6bb58208fd77aef55089f034abf739
jimit8929 <jimit8929@gmail.com>
2025-08-27 22:51:15 +0530

    Verify Email

--- Backend/Routes/auth.routes.js
+++ Backend/Routes/auth.routes.js
@@ -1,12 +1,18 @@
 import express from "express";
-import { login, logout, signup , verifyEmail} from "../Controllers/auth.controller.js";
+import { login, logout, signup , verifyEmail , forgotPassword , resetPassword, checkAuth} from "../Controllers/auth.controller.js";
+import { verifyAuth } from "../middlewares/verifyAuth.js";
 
 const router = express.Router();
+
+router.get("/check-auth" , verifyAuth , checkAuth);
 
 router.post("/signup" , signup);
 router.post("/login" , login);
 router.post("/logout" , logout);
 
 router.post("/verify_email" , verifyEmail);
+router.post("/forgot_password" , forgotPassword);
+router.post("/reset-password/:token" , resetPassword);
+
 
 export default router;

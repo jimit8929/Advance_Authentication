d27706fd6c4b5e0030c2377d789c45dc6a46115f
jimit8929 <jimit8929@gmail.com>
2025-08-31 16:52:45 +0530

    signup page ui

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,57 +1,29 @@
 import React from "react";
-
-//Components
-import FloatingShapes from "./components/FloatingShapes";
-
-
-//Pages
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Pages/Home";
 import SignUpPage from "./Pages/SignUpPage";
 import LoginPage from "./Pages/LoginPage";
-import EmailVerificationPage from "./Pages/EmailVerificationPage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
 
-import {Route , Routes} from "react-router-dom";
-import {Toaster} from "react-hot-toast";
-
+  {
+    path: "/signup",
+    element: <SignUpPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+]);
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
-
-
-      <FloatingShapes
-        color="bg-green-500"
-        size="w-64 h-64"
-        top="-5%"
-        left="10%"
-        delay={0}
-      />
-
-      <FloatingShapes
-        color="bg-emerald-500"
-        size="w-48 h-48"
-        top="70%"
-        left="80%"
-        delay={5}
-      />
-
-      <FloatingShapes
-        color="bg-lime-500"
-        size="w-32 h-32"
-        top="40%"
-        left="-10%"
-        delay={2}
-      />
-
-
-      <Routes>
-          <Route path="/" element={"Home"}/>
-          <Route path="/signup" element={<SignUpPage/>}/>
-          <Route path="/login" element={<LoginPage/>}/>
-          <Route path="/verify-email" element={<EmailVerificationPage/>}/>
-      </Routes>
-
-      <Toaster/>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 };

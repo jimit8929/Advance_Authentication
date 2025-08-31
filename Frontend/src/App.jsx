f7d58a8f74f67d5c8651fdb179b134ee93dc3ae1
jimit8929 <jimit8929@gmail.com>
2025-08-31 17:49:00 +0530

    Signup Completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+
+//PAGES
 import Home from "./Pages/Home";
 import SignUpPage from "./Pages/SignUpPage";
 import LoginPage from "./Pages/LoginPage";
+import VerifyEmailPage from "./Pages/EmailVerificationPage";
+import Verify from "./Pages/Verify";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +19,17 @@ const router = createBrowserRouter([
     path: "/signup",
     element: <SignUpPage />,
   },
+
+  {
+    path : "/verify",
+    element: <VerifyEmailPage/>
+  },
+
+  {
+    path : "/verify/:token",
+    element: <Verify/>
+  },
+
   {
     path: "/login",
     element: <LoginPage />,
